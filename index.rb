062a4e22ae7bb56dc7c72f2d1cbe2d4d2884764d
ironest <ironest@gmail.com>
2019-10-02 00:18:24 +1000

    Added refactori of API consumptio

--- index.rb
+++ index.rb
@@ -1,44 +1,84 @@
 require "httparty"
 require "JSON"
-
+require "open-uri"
 
 quiz = {
     name: "",
     types: [],
-    answer: "",
-    image: ""
+    answer: ""
 }
 
-base_api = "https://pokeapi.co/api/v2"
-pokemon_endpoint = "/pokemon/"
-type_endpoint = "/type/"
-
-pokemon_id = rand(1..150)
-final_uri = base_api +
-            pokemon_endpoint +
-            pokemon_id.to_s
-response = HTTParty.get(
-    final_uri,
-    {
-        body: {
-        },
-        headers: {
-        }
-    }
-)
-
-if (response.code != 200)
-    puts "Something went wrong"
-    exit
+def get_user_input
+    while true do    
+        answer = gets.chomp.to_i
+        if (1..3).include?(answer)
+            return answer - 1
+        else 
+            print "Please enter a valid input: "
+        end
+    end
 end
 
-data = JSON.parse(response.body)
+def get_pokemon_data
+
+    base_api = "https://pokeapi.co/api/v2"
+    endpoint = "/pokemon/"
+    pokemon_id = rand(1..150).to_s
+
+    final_uri = base_api +
+                endpoint +
+                pokemon_id
+
+    response = HTTParty.get(
+        final_uri,
+        {
+            body: {
+            },
+            headers: {
+            }
+        }
+    )
+    
+    if (response.code != 200)
+        puts "Something went wrong while querying the API"
+        exit
+    end
+
+    return JSON.parse(response.body)
+
+end
+
+data = get_pokemon_data
 
 quiz[:name] = data["species"]["name"]
 quiz[:types].push data["types"][0]["type"]["name"]
 quiz[:answer] = data["types"][0]["type"]["name"]
-quiz[:image] = data["sprites"]["front_default"]
 
-open("image.png", "wb") do |file|
-    file << open(quiz[:image]).read
+while (quiz[:types].length < 3)
+
+    data = get_pokemon_data
+
+    type = data["types"][0]["type"]["name"]
+
+    if not quiz[:types].include?(type)
+        quiz[:types].push type
+    end
+
 end
+
+quiz[:types].shuffle!
+
+puts "What is #{quiz[:name].capitalize} type? "
+puts " 1. #{quiz[:types][0].capitalize}"
+puts " 2. #{quiz[:types][1].capitalize}"
+puts " 3. #{quiz[:types][2].capitalize}"
+
+
+guess = get_user_input
+
+if quiz[:answer] == quiz[:types][guess]
+    puts "Correct!"
+else
+    puts "Nope, #{quiz[:name]} is a #{quiz[:answer]} pokemon"
+end
+
