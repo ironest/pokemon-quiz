c7f2d8d65fe7b2d989a1a11d1721111da8ba65de
ironest <ironest@gmail.com>
2019-10-01 23:37:31 +1000

    Added code to assemble the complete uri and the API consumption snippet

--- index.rb
+++ index.rb
@@ -13,3 +13,21 @@ base_api = "https://pokeapi.co/api/v2"
 pokemon_endpoint = "/pokemon/"
 type_endpoint = "/type/"
 
+pokemon_id = rand(1..150)
+final_uri = base_api +
+            pokemon_endpoint +
+            pokemon_id.to_s
+response = HTTParty.get(
+    final_uri,
+    {
+        body: {
+        },
+        headers: {
+        }
+    }
+)
+
+if (response.code != 200)
+    puts "Something went wrong"
+    exit
+end
